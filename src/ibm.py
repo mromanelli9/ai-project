aa26ebcf9e331e4949d8b5d40d545ed7cd53087a
Marco Romanelli <mromanelli9@gmail.com>
2017-06-15 10:14:21 +0200

    aggiunto file

---
+++ src/ibm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+## -*- Mode: python; py-indent-offset: 4; indent-tabs-mode: t; tab-width: 4; coding: utf-8; -*-
+#title           : ibm.py
+#description     : Uses IBM Vision Recognition API to analyze an image.
+#author          : Marco Romanelli
+#date            : 15/06/2017
+#version         : 1
+#python_version  : 3.6.0
+#==============================================================================
+
+def main(argv):
+
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
